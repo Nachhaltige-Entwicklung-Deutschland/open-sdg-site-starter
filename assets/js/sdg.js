07bba3d9953091cdaa54865fb0068968904e638b
MoBosse <moritz.bosse@destatis.de>
2019-10-08 12:04:32 +0000

    Automated deployment to GitHub Pages: 25cebdaf811c7ee3a86e65b538a30e3e28592b74

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1735,7 +1735,7 @@ var indicatorView = function (model, options) {
     };
     chartConfig = opensdg.chartConfigAlter(chartConfig);
 
-    this._chartInstance = new Chart($(this._rootElement).find('canvas'), chartConfig);
+    this._chartInstance = new CanvasJS.Chart($(this._rootElement).find('canvas'), chartConfig);
 
     Chart.pluginService.register({
       afterDraw: function(chart) {
