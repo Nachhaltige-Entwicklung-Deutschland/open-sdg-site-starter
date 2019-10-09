f0547ab908f5e8a3a71fe20d1df2db04a22d86fc
nachhaltige-entwicklung-deutschland <dns-indikatoren@destatis.de>
2019-10-09 13:50:19 +0200

    pointstyle 4.2

--- _includes/assets/js/indicatorModel.js
+++ _includes/assets/js/indicatorModel.js
@@ -468,7 +468,7 @@ var indicatorModel = function (options) {
         //   }) : undefined,
         var fieldIndex,
           ds = _.extend({
-            label: that.years ? that.years : that.country, //combinationDescription ? combinationDescription : that.country,
+            label: that.years[0] ? that.years[0] : that.country, //combinationDescription ? combinationDescription : that.country,
             borderColor: '#' + getColor(datasetIndex),
             backgroundColor: '#' + getColor(datasetIndex),
             pointStyle: getPointStyle(),
