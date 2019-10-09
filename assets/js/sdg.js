cbf6c2bdbb396a45bd260f3ee3ec3565be811eca
MoBosse <moritz.bosse@destatis.de>
2019-10-09 06:41:02 +0000

    Automated deployment to GitHub Pages: 03a1aff70e0d9c8e62357be20fc036f6dcc4c608

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -683,6 +683,7 @@ var indicatorDataStore = function(dataUrl) {
   this.chartTitle = options.chartTitle;
   this.graphType = options.graphType;
   this.measurementUnit = options.measurementUnit;
+  this.copyright = options.copyright;
   this.dataSource = options.dataSource;
   this.geographicalArea = options.geographicalArea;
   this.footnote = options.footnote;
@@ -710,6 +711,7 @@ var indicatorDataStore = function(dataUrl) {
     };
 
     that.years = extractUnique('Year');
+    that.display = extractUnique('Display');
 
     if(that.data[0].hasOwnProperty('GeoCode')) {
       that.hasGeoData = true;
@@ -827,13 +829,67 @@ var indicatorDataStore = function(dataUrl) {
     that.footerFields[translations.indicator.source] = that.dataSource;
     that.footerFields[translations.indicator.geographical_area] = that.geographicalArea;
     that.footerFields[translations.indicator.unit_of_measurement] = that.measurementUnit;
+    that.footerFields[translations.indicator.copyright] = that.copyright;
     that.footerFields[translations.indicator.footnote] = that.footnote;
     // Filter out the empty values.
     that.footerFields = _.pick(that.footerFields, _.identity);
   }());
 
   var headlineColor = '777777';
-  var colors = ['7e984f', '8d73ca', 'aaa533', 'c65b8a', '4aac8d', 'c95f44'];
+  if (this.indicatorId.includes('_1-')){
+    var colors = ['e5243b', '891523', 'ef7b89', '2d070b', 'f4a7b0', 'b71c2f', 'ea4f62', '5b0e17', 'fce9eb'];
+  }
+  else if(this.indicatorId.includes('_2-')){
+    var colors = ['e5b735', '896d1f', 'efd385', '2d240a', 'f4e2ae', 'b7922a', 'eac55d', '5b4915', 'f9f0d6'];
+  }
+  else if(this.indicatorId.includes('_3-')){
+    var colors = ['4c9f38', '2d5f21', '93c587', '0f1f0b', 'c9e2c3', '3c7f2c', '6fb25f', '1e3f16', 'a7d899'];
+  }
+  else if(this.indicatorId.includes('_4-')){
+    var colors = ['c5192d', '760f1b', 'dc7581', '270509', 'f3d1d5', '9d1424', 'd04656', '4e0a12', 'e7a3ab'];
+  }
+  else if(this.indicatorId.includes('_5-')){
+    var colors = ['ff3a21', 'b22817', 'ff7563', '330b06', 'ffd7d2', 'cc2e1a', 'ff614d', '7f1d10', 'ff9c90'];
+  }
+  else if(this.indicatorId.includes('_6-')){
+    var colors = ['26bde2', '167187', '7cd7ed', '07252d', 'd3f1f9', '1e97b4', '51cae7', '0f4b5a', 'a8e4f3'];
+  }
+  else if(this.indicatorId.includes('_7-')){
+    var colors = ['fcc30b', '977506', 'fddb6c', '322702', 'fef3ce', 'c99c08', 'fccf3b', '644e04', 'fde79d'];
+  }
+  else if(this.indicatorId.includes('_8-')){
+    var colors = ['a21942', '610f27', 'c7758d', 'a21942', 'ecd1d9', '811434', 'b44667', '400a1a', 'd9a3b3'];
+  }
+  else if(this.indicatorId.includes('_9-')){
+    var colors = ['fd6925', '973f16', 'fda57c', '321507', 'fee1d3', 'ca541d', 'fd8750', '652a0e', 'fec3a7'];
+  }
+  else if(this.indicatorId.includes('_10-')){
+    var colors = ['dd1367', '840b3d', 'ea71a3', '2c0314', 'f8cfe0', 'b00f52', 'd5358b', '580729', 'f1a0c2'];
+  }
+  else if(this.indicatorId.includes('_11-')){
+    var colors = ['fd9d24', '653e0e', 'fed7a7', 'b16d19', 'fdba65', 'b14a1e', 'fd976b', '000000', 'fed2bf'];
+  }
+  else if(this.indicatorId.includes('_12-')){
+    var colors = ['c9992d', '785b1b', 'dec181', '281e09', 'f4ead5', 'a07a24', 'd3ad56', '503d12', 'e9d6ab'];
+  }
+  else if(this.indicatorId.includes('_13-')){
+    var colors = ['3f7e44', '254b28', '8bb18e', '0c190d', 'd8e5d9', '326436', '659769', '19321b', 'b2cbb4'];
+  }
+  else if(this.indicatorId.includes('_14-')){
+    var colors = ['0a97d9', '065a82', '6cc0e8', '021e2b', 'ceeaf7', '0878ad', '3aabe0', '043c56', '9dd5ef'];
+  }
+  else if(this.indicatorId.includes('_15-')){
+    var colors = ['56c02b', '337319', '99d97f', '112608', 'ddf2d4', '449922', '77cc55', '224c11', 'bbe5aa'];
+  }
+  else if(this.indicatorId.includes('_16-')){
+    var colors = ['00689d', '00293e', '99c2d7', '00486d', '4c95ba', '126b80', 'cce0eb', '5a9fb0', 'a1c8d2'];
+  }
+  else if(this.indicatorId.includes('_17-')){
+    var colors = ['19486a', '0a1c2a', '8ca3b4', '16377c', 'd1dae1', '11324a', '466c87', '5b73a3', '0f2656'];
+  }
+  //SDG goal colors
+  //['e5243b', 'e5b735', '4c9f38', 'c5192d', 'ff3a21', '26bde2', 'fcc30b', 'a21942', 'fd6925', 'dd1367'];
+
 
   // allow headline + (2 x others)
   var maxDatasetCount = 2 * colors.length;
@@ -1037,10 +1093,17 @@ var indicatorDataStore = function(dataUrl) {
         //     return f === field;
         //   }) : undefined,
         var fieldIndex,
+        var ps = undefined,
+        if (that.display === 'b) target'){
+          ps = 'star';
+        } else {
+          ps = 'circle';
+        }
           ds = _.extend({
             label: combinationDescription ? combinationDescription : that.country,
             borderColor: '#' + getColor(datasetIndex),
             backgroundColor: '#' + getColor(datasetIndex),
+            pointStyle: ps,
             pointBorderColor: '#' + getColor(datasetIndex),
             borderDash: getBorderDash(datasetIndex),
             data: _.map(that.years, function (year) {
@@ -1672,7 +1735,6 @@ var indicatorView = function (model, options) {
     var chartConfig = {
       type: this._model.graphType,
       data: chartInfo,
-      pointStyle: 'cross',
       options: {
         responsive: true,
         maintainAspectRatio: false,
