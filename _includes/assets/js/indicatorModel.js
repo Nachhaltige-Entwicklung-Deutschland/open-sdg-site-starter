1bfc54c344f703a534b966d384ffc6de2770ff45
nachhaltige-entwicklung-deutschland <dns-indikatoren@destatis.de>
2019-10-09 09:05:39 +0200

    pointstyle 0.1

--- _includes/assets/js/indicatorModel.js
+++ _includes/assets/js/indicatorModel.js
@@ -243,8 +243,8 @@ var indicatorModel = function (options) {
   }
   //SDG goal colors
   //['e5243b', 'e5b735', '4c9f38', 'c5192d', 'ff3a21', '26bde2', 'fcc30b', 'a21942', 'fd6925', 'dd1367'];
-  var headlinePointstyle: 'circle'
-  var pointStyles = ['circle', 'cross', 'crossRot', 'dash', 'line', 'rect', 'rectRounded', 'rectRot', 'star', 'triangle']
+  var headlinePointstyle: 'circle';
+  var pointStyles = ['circle', 'cross', 'crossRot', 'dash', 'line', 'rect', 'rectRounded', 'rectRot', 'star', 'triangle'];
 
   // allow headline + (2 x others)
   var maxDatasetCount = 2 * colors.length;
