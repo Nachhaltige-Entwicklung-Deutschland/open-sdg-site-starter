6543d1685e703bbaca16d993338532056cc25c0a
MoBosse <moritz.bosse@destatis.de>
2019-10-09 13:28:29 +0000

    Automated deployment to GitHub Pages: e204121563b6c71de51cd67d719036ec293bcebd

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1093,10 +1093,10 @@ var indicatorDataStore = function(dataUrl) {
       */
 
       getPointStyle = function (combinationDescription) {
-        if ((String(combinationDescription).indexOf('Ziel (Minimum)') != -1) || (String(combinationDescription).indexOf('Target (Minimum)') != -1)){
+        if ((String(combinationDescription).indexOf('Ziel (Minimum)') != -1){// || (String(combinationDescription).indexOf('Target (Minimum)') != -1)){
           return 'triangle';
         }
-        else if ((String(combinationDescription).indexOf('Ziel (Maximum)') != -1) || (String(combinationDescription).indexOf('Target (Maximum)') != -1)){
+        else if ((String(combinationDescription).indexOf('Ziel (Maximum)') != -1){// || (String(combinationDescription).indexOf('Target (Maximum)') != -1)){
           return 'cross';
         }
         else {
