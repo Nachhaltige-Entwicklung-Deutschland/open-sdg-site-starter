923370ddc747917923142eac085a95628e111c0f
MoBosse <moritz.bosse@destatis.de>
2019-10-14 12:09:19 +0000

    Automated deployment to GitHub Pages: 4b56e41a245d08e779da95e0bad80e38a77b0421

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2413,6 +2413,7 @@ $(function() {
     },
 
     onAdd: function() {
+      var plugin = this.plugin;
       var name1 = plugin.getData(selection.feature.properties.name);
       var controlTpl = '{name1}' +
         '<ul id="selection-list"></ul>' +
