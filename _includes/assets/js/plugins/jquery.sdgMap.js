da81c93108e858a9f4bcb093ef4be093c4fc0f97
nachhaltige-entwicklung-deutschland <dns-indikatoren@destatis.de>
2019-10-14 11:16:59 +0200

    test maps 1.2

--- _includes/assets/js/plugins/jquery.sdgMap.js
+++ _includes/assets/js/plugins/jquery.sdgMap.js
@@ -232,7 +232,15 @@
       L.tileLayer(this.options.tileURL, this.options.tileOptions).addTo(this.map);
 
       //-------------------------------------------
-      this.info = L.control('Hallo');
+      this.info = L.control();
+      this.info = function (map){
+        this._div = L.DomUtil.create('div','info');
+        this.update();
+        return this._div;
+      };
+      this.info.update = function (props) {
+        this._div.innerHTML = 'Hallo';
+      };
       this.info.addTo(this.map);
 
 
