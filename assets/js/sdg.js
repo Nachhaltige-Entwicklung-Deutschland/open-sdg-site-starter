498fa5e20bdaeaebc90b6315bcfe3a666564ada2
MoBosse <moritz.bosse@destatis.de>
2019-10-23 09:18:46 +0000

    Automated deployment to GitHub Pages: 7d70d05268c0937abfbfb673776be6f388e6ebe5

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1769,7 +1769,6 @@ var indicatorView = function (model, options) {
   };
 
 
-  Chart.defaults.global.legend.labels.usePointStyle = true;
   this.createPlot = function (chartInfo) {
 
     var that = this;
