2a22922a28922f43fdedbe7964477ec8f12aa44b
MoBosse <moritz.bosse@destatis.de>
2019-10-25 09:12:13 +0000

    Automated deployment to GitHub Pages: d915a64a5e9cc560621a7e9110702a9e07d2a992

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1152,16 +1152,19 @@ var indicatorDataStore = function(dataUrl) {
         }
 
         else{
+          var tmpIndex = nameList.indexOf(combinationDescription.substring(combinationDescription.indexOf(','), combinationDescription.length));
+          var tsrIndex = indexList[tmpIndex]
+
           var fieldIndex,
             ds = _.extend({
 
               label: combinationDescription ? combinationDescription : that.country,
-              borderColor: '#000000',//' + getColor(datasetIndex),
-              backgroundColor: '#000000',// + getColor(datasetIndex),
+              borderColor: '#' + getColor(tsrIndex),
+              backgroundColor: '#' + getColor(tsrIndex),
               pointStyle: getPointStyle(combinationDescription),
               radius: 8,
               rotation: 180,
-              pointBorderColor: '#000000',//' + getColor(datasetIndex),
+              pointBorderColor: '#' + getColor(tsrIndex),
               borderDash: getBorderDash(datasetIndex),
               data: _.map(that.years, function (year) {
                 var found = _.findWhere(data, {
