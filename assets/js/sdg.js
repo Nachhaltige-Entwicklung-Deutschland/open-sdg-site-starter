790a7b91b544c9294550628d9e04110ebe8e26b3
MoBosse <moritz.bosse@destatis.de>
2019-10-25 08:01:45 +0000

    Automated deployment to GitHub Pages: bea87518921a6ea06bb855bb6853f12017f9dd3d

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1123,7 +1123,7 @@ var indicatorDataStore = function(dataUrl) {
             //var colorCheck =
             //---------------------
 
-            label: combinationDescription ? combinationDescription.substring(combinationDescription.indexOff(','), combinationDescription.length) : that.country,
+            label: combinationDescription ? combinationDescription.substring(combinationDescription.indexOf(','), combinationDescription.length) : that.country,
             borderColor: '#' + getColor(datasetIndex),
             backgroundColor: '#' + getColor(datasetIndex),
             pointStyle: getPointStyle(combinationDescription),
