119802d8e51e710ca39be17a430db658fd631944
MoBosse <moritz.bosse@destatis.de>
2019-11-19 11:28:42 +0000

    Automated deployment to GitHub Pages: 9c88f58775e611c265286fa7bda968bb7c2c8707

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -147,7 +147,7 @@ opensdg.autotrack = function(preset, category, action, label) {
         // First add the time series data.
         var records = _.where(geoData, { GeoCode: geocode });
         if (records.sex){
-          records = _.where(geoData, { sex: 'female' });
+          records = _.where(records, { sex: 'female' });
         }
         records.forEach(function(record) {
           // Add the Year data into the properties.
