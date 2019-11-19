1e5f0ebeaf9a3ac85c312b93cff5f3e29a0e317a
nachhaltige-entwicklung-deutschland <dns-indikatoren@destatis.de>
2019-11-19 12:09:50 +0100

    1.1

--- _includes/assets/js/plugins/jquery.sdgMap.js
+++ _includes/assets/js/plugins/jquery.sdgMap.js
@@ -114,12 +114,15 @@
 
 
         // First add the time series data.
-        var records = _.where(geoData, { GeoCode: geocode });
+        var records1 = _.where(geoData, { GeoCode: geocode });
+        var records = _.where(records1, { sex: 'female' });
         records.forEach(function(record) {
           // Add the Year data into the properties.
-          //feature.properties[record.Year] = record.Value;
-          feature.properties.time = record.Year;
-          feature.properties.density = record.Value;
+          feature.properties[record.Year] = record.Value;
+          //----------------
+          //feature.properties.time = record.Year;
+          //feature.properties.density = record.Value;
+          //------------------
 
         });
 
