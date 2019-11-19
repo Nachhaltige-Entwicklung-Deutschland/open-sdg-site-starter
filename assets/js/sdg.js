c2e9ff28f697c1eb184a08897a46c3a22332fac5
MoBosse <moritz.bosse@destatis.de>
2019-11-19 15:33:42 +0000

    Automated deployment to GitHub Pages: 4f9600971f654706c1448345cd1e4980177b4402

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -315,14 +315,15 @@ opensdg.autotrack = function(preset, category, action, label) {
       var expressions = ['Männer', 'Frauen']
       // create the control
       for (var exp in expressions) {
-        var command = L.control({position: 'bottomright'});
-        command.onAdd = function (map) {
-            var div = L.DomUtil.create('div', 'command');
-            div.innerHTML = '<form><input id="command" type="checkbox"/> '+expressions[exp]+'</form>';
-            return div;
-        };
-        command.addTo(this.map);
-        //document.getElementById ("command").addEventListener ("click", handleCommand(expressions[exp]), false);
+        if  (expressions[exp] != 'function(val) { return this.indexOf(val) != -1; }')
+          var command = L.control({position: 'bottomright'});
+          command.onAdd = function (map) {
+              var div = L.DomUtil.create('div', 'command');
+              div.innerHTML = '<form><input id="command" type="checkbox"/> '+expressions[exp]+'</form>';
+              return div;
+          };
+          command.addTo(this.map);
+          //document.getElementById ("command").addEventListener ("click", handleCommand(expressions[exp]), false);
       }
 
 
