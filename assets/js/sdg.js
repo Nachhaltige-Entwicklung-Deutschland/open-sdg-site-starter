5fba34a21004da972f28a782c6e3f9e79237e5d0
MoBosse <moritz.bosse@destatis.de>
2019-11-20 16:22:11 +0000

    Automated deployment to GitHub Pages: ff63581153f8d951eb90aaf6ba24fb537a91cfce

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -164,6 +164,9 @@ opensdg.autotrack = function(preset, category, action, label) {
     //Find those disaggregation-categories that have more then one expression in all lines that have geoData
     findCat: function(){
       var categories = ['title','sex','age','typification','criminal offences'];
+      for (var i; i<categories.length; i++){
+        var tmpExp = _.pluck(this.geoData, categories[i]);
+      };
       var category = 'sex';
       return category;
     },
@@ -353,7 +356,7 @@ opensdg.autotrack = function(preset, category, action, label) {
         var command = L.control({position: 'bottomright'});
         command.onAdd = function (map) {
             var div = L.DomUtil.create('div', 'command');
-            if (i == 0){
+            if (i == 1){
               div.innerHTML = '<input id="command'+i+'" type="radio" name="disagg" value="'+exp[i]+'" checked> '+exp[i]+'<br>';
             }
             else{
