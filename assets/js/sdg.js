ce23ad1ff259f5df6f7e1c0f5a89e714a2d13735
MoBosse <moritz.bosse@destatis.de>
2019-11-20 15:55:14 +0000

    Automated deployment to GitHub Pages: f643b5bcc3cc2518c6f5d0b400c2b371f088dcbb

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -163,18 +163,28 @@ opensdg.autotrack = function(preset, category, action, label) {
     //---------------------------
     //Find those disaggregation-categories that have more then one expression in all lines that have geoData
     findCat: function(){
-      var category = 'sex';
-      return category;
+      var categories = ['title', 'sex', 'age', 'typification', 'criminal offences'];
+      for (var i = 0; i<categories.length; i++){
+        var tempEx = _.pluck(this.geoData, categories[i] );
+        var unique = [ ...new Set(tempEx) ];
+        if (unique.length >1){
+          return categories[i];
+        }
+        else{
+          return 'sex';
+        }
+      }
     },
 
 
-
+    //Find all expressions of the given categorie in all lines that have geoData
     findDisagg: function(){
-      var expressions = _.pluck(this.geoData, 'sex');
+      var expressions = _.pluck(this.geoData, plugin.findCat());
       eq = [ ...new Set(expressions) ];
       return eq;
     },
 
+    // Get the coosen expression from the radio buttons
     getExpression: function(){
       var expression = $('input[name="disagg"]:checked').val();
       return expression;
@@ -205,7 +215,7 @@ opensdg.autotrack = function(preset, category, action, label) {
     },
 
     */
-    //---------------------------
+    //-------------------------------------------------------------------------------------------------------
 
     // Zoom to a feature.
     zoomToFeature: function(layer) {
