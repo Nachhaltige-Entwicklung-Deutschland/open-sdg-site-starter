9eb447133d078b7c7686a7d59eb2acd5c8c7f01c
MoBosse <moritz.bosse@destatis.de>
2019-11-20 12:59:25 +0000

    Automated deployment to GitHub Pages: 127bf58f05fdb42a6727f3fdd31c6c81fccac737

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -343,7 +343,7 @@ opensdg.autotrack = function(preset, category, action, label) {
         var command = L.control({position: 'bottomright'});
         command.onAdd = function (map) {
             var div = L.DomUtil.create('div', 'command');
-            div.innerHTML = '<form><input id="command'+i+'" type="radio" name="disagg"/> '+exp[i]+'</form>';
+            div.innerHTML = '<input id="command'+i+'" type="radio" name="disagg" value="'+exp[i]+'"> '+exp[i]+'<br>';
             return div;
         };
         command.addTo(this.map);
