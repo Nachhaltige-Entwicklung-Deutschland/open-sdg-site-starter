0f5003c190839f119cfc80a3394b20cdcc77b389
MoBosse <moritz.bosse@destatis.de>
2019-11-20 10:25:03 +0000

    Automated deployment to GitHub Pages: 761622761c29dd4e7bbe1c25a4c88e692b2cd691

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -388,8 +388,8 @@ opensdg.autotrack = function(preset, category, action, label) {
           var idProperty = plugin.mapLayers[i].idProperty;
           var nameProperty = plugin.mapLayers[i].nameProperty;
 
-          var cat = findCat()
-          var exp = findDisagg(geoJsons)
+          var cat = findCat();
+          var exp = findDisagg(geoJsons);
           var geoJson = plugin.prepareGeoJson(geoJsons[i][0], idProperty, nameProperty, cat, exp);
 
           var layer = L.geoJson(geoJson, {
