5bd2d2e43b121e404fce6a147a94eb4ca44eea81
nachhaltige-entwicklung-deutschland <dns-indikatoren@destatis.de>
2019-11-20 15:38:41 +0100

    refresh 1

--- _includes/assets/js/plugins/jquery.sdgMap.js
+++ _includes/assets/js/plugins/jquery.sdgMap.js
@@ -318,7 +318,7 @@
         var command = L.control({position: 'bottomright'});
         command.onAdd = function (map) {
             var div = L.DomUtil.create('div', 'command');
-            if (i == 2){
+            if (i == 1){
               div.innerHTML = '<input id="command'+i+'" type="radio" name="disagg" value="'+exp[i]+'" checked> '+exp[i]+'<br>';
             }
             else{
