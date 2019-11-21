54d0f8245219592ff8a62ce95131135bc01dd1f7
MoBosse <moritz.bosse@destatis.de>
2019-11-21 08:00:22 +0000

    Automated deployment to GitHub Pages: 6d07945f6fbd4f9e0a5e393014bef94cb2aa2fc4

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -171,7 +171,7 @@ opensdg.autotrack = function(preset, category, action, label) {
           var category = caseategories[i];
         }
       };
-      
+
       return category;
     },
 
@@ -360,7 +360,7 @@ opensdg.autotrack = function(preset, category, action, label) {
         var command = L.control({position: 'bottomright'});
         command.onAdd = function (map) {
             var div = L.DomUtil.create('div', 'command');
-            if (i == 0){
+            if (i == 1){
               div.innerHTML = '<input id="command'+i+'" type="radio" name="disagg" value="'+exp[i]+'" checked> '+exp[i]+'<br>';
             }
             else{
