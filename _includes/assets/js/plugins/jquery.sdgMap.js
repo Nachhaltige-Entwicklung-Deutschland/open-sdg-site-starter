de30d543359029bb96c6439a2ed0ce0843902fd7
nachhaltige-entwicklung-deutschland <dns-indikatoren@destatis.de>
2019-11-21 10:58:09 +0100

    10.9

--- _includes/assets/js/plugins/jquery.sdgMap.js
+++ _includes/assets/js/plugins/jquery.sdgMap.js
@@ -322,7 +322,7 @@
 
       //------------------------------------------------------------------------------------------------------------------------
       var exp = plugin.findDisagg(plugin.findCat());
-
+      div.innerHTML = '<form><fieldset>'
       for (var i = 0; i<exp.length; i++) {
         if (!exp[i]){
           var label = 'total';
@@ -341,6 +341,7 @@
             }
             return div;
         };
+        div.innerHTML = '</fieldset></form>'
         command.addTo(this.map);
         //document.getElementById ("command").addEventListener ("click", handleCommand(disaggs[i]), false);
       };
