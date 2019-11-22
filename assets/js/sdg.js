0cec69bdd537e881f3892df32c90b3fc42a21608
MoBosse <moritz.bosse@destatis.de>
2019-11-22 11:18:29 +0000

    Automated deployment to GitHub Pages: 1859af8aa2c617058a3c9d5f46c4e93687b8bd6c

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -140,7 +140,19 @@ opensdg.autotrack = function(preset, category, action, label) {
 
   Plugin.prototype = {
 
+    //Find those disaggregation-categories that have more then one expression in all lines that have geoData
+    findCat: function(){
+      var categories = ['title','sex','age'];
+      var category = '';
 
+      for (var i = 0; i<categories.length; i++){
+        if (this.findDisagg(categories[i]).length>1){ //if more then one expression for this categorie exists...
+          var category = categories[i];
+          this.hasMapDisaggs = true;
+        }
+      };
+      return category;
+    },
 
     // Add time series to GeoJSON data and normalize the name and geocode.
     prepareGeoJson: function(geoJson, idProperty, nameProperty, cat, exp) { //--------------------------------added cat & exp
@@ -174,19 +186,7 @@ opensdg.autotrack = function(preset, category, action, label) {
     },
 
     //---------------------------
-    //Find those disaggregation-categories that have more then one expression in all lines that have geoData
-    findCat: function(){
-      var categories = ['title','sex','age'];
-      var category = '';
 
-      for (var i = 0; i<categories.length; i++){
-        if (this.findDisagg(categories[i]).length>1){ //if more then one expression for this categorie exists...
-          var category = categories[i];
-          this.hasMapDisaggs = true;
-        }
-      };
-      return category;
-    },
 
     // Get the found category and return an array with the corresponding expressions
     findDisagg: function(category){
