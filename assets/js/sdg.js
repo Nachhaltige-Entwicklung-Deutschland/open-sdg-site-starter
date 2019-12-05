456405e354aba04dda4d956bd2d02ae7283de06b
MoBosse <moritz.bosse@destatis.de>
2019-12-05 08:23:04 +0000

    Automated deployment to GitHub Pages: 4398d8e6970fdccf5c5c4473146c0206fda02611

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1911,6 +1911,7 @@ var indicatorView = function (model, options) {
 
   this.updatePlot = function(chartInfo) {
     view_obj._chartInstance.data.datasets = chartInfo.datasets;
+    console.log (chartInfo);
 
     if(chartInfo.selectedUnit) {
       view_obj._chartInstance.options.scales.yAxes[0].scaleLabel.labelString = translations.t(chartInfo.selectedUnit);
@@ -1935,6 +1936,7 @@ var indicatorView = function (model, options) {
 
 
   this.createPlot = function (chartInfo) {
+    //for set in chartInfo.datasets
     console.log (chartInfo);
     var that = this;
     var chartConfig = {
@@ -1986,18 +1988,6 @@ var indicatorView = function (model, options) {
             text.push('</ul>');
             return text.join('');
         },
-
-
-        //showLines: function(chart) {
-          //var showLine= false;
-          //_.each(chart.data.datasets, function(dataset, datasetIndex) {
-            //if (dataset.label.substr(0,4) == 'Ziel' || dataset.label.substr(0,6) == 'Target'){
-              //showline = false;
-            //}
-          //});
-          //return showline;
-        //true},
-
 
         legend: {
           display: false,
