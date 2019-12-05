63ad0dd049fe3d5bced290909abf9485a90faabe
MoBosse <moritz.bosse@destatis.de>
2019-12-05 08:07:58 +0000

    Automated deployment to GitHub Pages: d9c2e854784aa4bc43f84e986b750410538e69a6

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1935,7 +1935,7 @@ var indicatorView = function (model, options) {
 
 
   this.createPlot = function (chartInfo) {
-    console.log (chartInfo);
+    console.log (chartInfo.datasets);
     var that = this;
     var chartConfig = {
       type: this._model.graphType,
