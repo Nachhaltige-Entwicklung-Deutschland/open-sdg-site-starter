880ddd9805d7d75d465ff0f78e75451d88ffe290
MoBosse <moritz.bosse@destatis.de>
2019-12-05 12:33:36 +0000

    Automated deployment to GitHub Pages: 8e5cf2e240013656c18ca2b5c926cae963ff995b

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1300,7 +1300,7 @@ var indicatorDataStore = function(dataUrl) {
               });
               return found ? found.Value : null;
             }),
-            type: this.graphType,
+            type: that.graphType,
             borderWidth: combinationDescription ? 2 : 4
           }, that.datasetObject);
         //----------------------------------
