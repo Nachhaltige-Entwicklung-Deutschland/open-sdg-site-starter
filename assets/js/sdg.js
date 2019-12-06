c8465529eeb2b33717d3a558d6011b499e6dd6c7
MoBosse <moritz.bosse@destatis.de>
2019-12-06 09:27:14 +0000

    Automated deployment to GitHub Pages: 24073b6c393928a3a91d054c1475265743602cf5

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -322,9 +322,9 @@ opensdg.autotrack = function(preset, category, action, label) {
 
       // mapbox logo.
       var logo = L.control({position: 'bottomleft'});
-      command.onAdd = function (map) {
+      logo.onAdd = function (map) {
         var div = L.DomUtil.create('div', 'logo');
-        div.innerHTML = '<a href:"https://www.mapbox.com"> <img src="https://g205sdgs.github.io/sdg-indicators/public/mapbox-logo-white.png"/> </a>'
+        div.innerHTML = '<a href:"https://mapbox.com"> <img src="https://g205sdgs.github.io/sdg-indicators/public/mapbox-logo-white.png"/> </a>'
         return div;
       };
       logo.addTo(this.map);
