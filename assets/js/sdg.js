9783f6fa2931890c3ddb9b361309767d644f6d21
MoBosse <moritz.bosse@destatis.de>
2019-12-10 13:15:23 +0000

    Automated deployment to GitHub Pages: c48f0bf630ea4873cb355d1b578f738dc027a98c

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -141,11 +141,11 @@ opensdg.autotrack = function(preset, category, action, label) {
     this.typificationName = translations.t(this.typification[this.typification.length -1]);
     this.criminalOffence = _.pluck(this.geoData, 'criminal offences');
     this.criminalOffenceName = translations.t(this.criminalOffence[this.criminalOffence.length -1]);
-    //---#6 enableMapsForDisagData---start-----------------------------------------------------------------
+    /*//---#6 enableMapsForDisagData---start-----------------------------------------------------------------
     this.startExp = 0;
     this.reloadCounter = 0; // to avoid multiple search buttons
     this.hasMapDisaggs = false;
-    //---#6 enableMapsForDisagData---stop------------------------------------------------------------------
+    *///---#6 enableMapsForDisagData---stop------------------------------------------------------------------
 
     this.init();
   }
