6b79da6d762dd97621fff91a01a576bc87a6a575
nachhaltige-entwicklung-deutschland <dns-indikatoren@destatis.de>
2019-12-10 14:11:02 +0100

    6.3

--- _includes/assets/js/plugins/jquery.sdgMap.js
+++ _includes/assets/js/plugins/jquery.sdgMap.js
@@ -458,7 +458,7 @@
         //---#6 enableMapsForDisagData---start-----------------------------------------------------------------
         //A reload due to Radio-button change creates a second search-Button.
         //Therefor we need to ask if it is the first load here:
-        if (plugin.reloadCounter == 1){
+        //if (plugin.reloadCounter == 1){
         //---#6 enableMapsForDisagData---stop------------------------------------------------------------------
           plugin.searchControl = new L.Control.Search({
             layer: plugin.getAllLayers(),
@@ -474,7 +474,7 @@
             autoCollapse: true,
           });
 
-        }//---#6 enableMapsForDisagData---start/stop-----------------------------------------------------------------
+        //}//---#6 enableMapsForDisagData---start/stop-----------------------------------------------------------------
         plugin.map.addControl(plugin.searchControl);
         // The search plugin messes up zoomShowHide, so we have to reset that
         // with this hacky method. Is there a better way?
