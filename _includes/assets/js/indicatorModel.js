c4795f6fe7390f9fec65f66937b728d0b411b4e7
nachhaltige-entwicklung-deutschland <dns-indikatoren@destatis.de>
2019-12-10 13:47:56 +0100

    ie #6.2

--- _includes/assets/js/indicatorModel.js
+++ _includes/assets/js/indicatorModel.js
@@ -189,55 +189,55 @@ var indicatorModel = function (options) {
   }());
 
   var headlineColor = '777777';
-  if (this.indicatorId.includes('_1-')){
+  if (this.indicatorId.indexOf('_1-') != -1){
     var colors = ['e5243b', '891523', 'ef7b89', '2d070b', 'f4a7b0', 'b71c2f', 'ea4f62', '5b0e17', 'fce9eb'];
   }
-  else if(this.indicatorId.includes('_2-')){
+  else if(this.indicatorId.indexOf('_2-') != -1){
     var colors = ['e5b735', '896d1f', 'efd385', '2d240a', 'f4e2ae', 'b7922a', 'eac55d', '5b4915', 'f9f0d6'];
   }
-  else if(this.indicatorId.includes('_3-')){
+  else if(this.indicatorId.indexOf('_3-') != -1){
     var colors = ['4c9f38', '2d5f21', '93c587', '0f1f0b', 'c9e2c3', '3c7f2c', '6fb25f', '1e3f16', 'a7d899'];
   }
-  else if(this.indicatorId.includes('_4-')){
+  else if(this.indicatorId.indexOf('_4-') != -1){
     var colors = ['c5192d', '760f1b', 'dc7581', '270509', 'f3d1d5', '9d1424', 'd04656', '4e0a12', 'e7a3ab'];
   }
-  else if(this.indicatorId.includes('_5-')){
+  else if(this.indicatorId.indexOf('_5-') != -1){
     var colors = ['ff3a21', 'b22817', 'ff7563', '330b06', 'ffd7d2', 'cc2e1a', 'ff614d', '7f1d10', 'ff9c90'];
   }
-  else if(this.indicatorId.includes('_6-')){
+  else if(this.indicatorId.indexOf('_6-') != -1){
     var colors = ['26bde2', '167187', '7cd7ed', '07252d', 'd3f1f9', '1e97b4', '51cae7', '0f4b5a', 'a8e4f3'];
   }
-  else if(this.indicatorId.includes('_7-')){
+  else if(this.indicatorId.indexOf('_7-') != -1){
     var colors = ['fcc30b', '977506', 'fddb6c', '322702', 'fef3ce', 'c99c08', 'fccf3b', '644e04', 'fde79d'];
   }
-  else if(this.indicatorId.includes('_8-')){
+  else if(this.indicatorId.indexOf('_8-') != -1){
     var colors = ['a21942', '610f27', 'c7758d', '610F28', 'ecd1d9', '811434', 'b44667', '400a1a', 'd9a3b3'];
   }
-  else if(this.indicatorId.includes('_9-')){
+  else if(this.indicatorId.indexOf('_9-') != -1){
     var colors = ['fd6925', '973f16', 'fda57c', '321507', 'fee1d3', 'ca541d', 'fd8750', '652a0e', 'fec3a7'];
   }
-  else if(this.indicatorId.includes('_10-')){
+  else if(this.indicatorId.indexOf('_10-') != -1){
     var colors = ['dd1367', '840b3d', 'ea71a3', '2c0314', 'f8cfe0', 'b00f52', 'd5358b', '580729', 'f1a0c2'];
   }
-  else if(this.indicatorId.includes('_11-')){
+  else if(this.indicatorId.indexOf('_11-') != -1){
     var colors = ['fd9d24', '653e0e', 'fed7a7', 'b16d19', 'fdba65', 'b14a1e', 'fd976b', '000000', 'fed2bf'];
   }
-  else if(this.indicatorId.includes('_12-')){
+  else if(this.indicatorId.indexOf('_12-') != -1){
     var colors = ['c9992d', '785b1b', 'dec181', '281e09', 'f4ead5', 'a07a24', 'd3ad56', '503d12', 'e9d6ab'];
   }
-  else if(this.indicatorId.includes('_13-')){
+  else if(this.indicatorId.indexOf('_13-') != -1){
     var colors = ['3f7e44', '254b28', '8bb18e', '0c190d', 'd8e5d9', '326436', '659769', '19321b', 'b2cbb4'];
   }
-  else if(this.indicatorId.includes('_14-')){
+  else if(this.indicatorId.indexOf('_14-')){
     var colors = ['0a97d9', '065a82', '6cc0e8', '021e2b', 'ceeaf7', '0878ad', '3aabe0', '043c56', '9dd5ef'];
   }
-  else if(this.indicatorId.includes('_15-')){
+  else if(this.indicatorId.indexOf('_15-') != -1){
     var colors = ['56c02b', '337319', '99d97f', '112608', 'ddf2d4', '449922', '77cc55', '224c11', 'bbe5aa'];
   }
-  else if(this.indicatorId.includes('_16-')){
+  else if(this.indicatorId.indexOf('_16-') != -1){
     var colors = ['00689d', '00293e', '99c2d7', '00486d', '4c95ba', '126b80', 'cce0eb', '5a9fb0', 'a1c8d2'];
   }
-  else if(this.indicatorId.includes('_17-')){
+  else if(this.indicatorId.indexOf('_17-') != -1){
     var colors = ['19486a', '0a1c2a', '8ca3b4', '16377c', 'd1dae1', '11324a', '466c87', '5b73a3', '0f2656'];
   };
   //SDG goal colors
@@ -473,7 +473,7 @@ var indicatorModel = function (options) {
                   translations.t('a) time series')+", "+translations.t('index overall'),
                   translations.t('b) target (min)')+", "+translations.t('index overall')]
       getChartStyle = function (combinationDescription) {
-        if (barCharts.includes(String(combinationDescription))) {
+        if (barCharts.indexOf(String(combinationDescription)) != -1) {
           return 'bar';
         }
         else {
