0b19422a33b8522fe03633d8986f17b815bce4ba
MoBosse <moritz.bosse@destatis.de>
2019-12-16 08:36:29 +0000

    Automated deployment to GitHub Pages: d560bfb476cc89628fda180b1546e4544831b25b

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2103,8 +2103,9 @@ var indicatorView = function (model, options) {
               }
               //---#3 targetDifferentInLegend---stop-----------------------------------------------------------------------------------------------------------------------------
               text.push('</span>');
-              text.push(translations.t(set.label),'<br>');
+              text.push(translations.t(set.label));
               text.push('</li>');
+              text.push('<hr>');
             });
 
             text.push('</ul>');
