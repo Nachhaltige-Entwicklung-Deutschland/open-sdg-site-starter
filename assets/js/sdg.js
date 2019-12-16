787a5da901914a55fa1d5d8cc20fa5b79eeaae6f
MoBosse <moritz.bosse@destatis.de>
2019-12-16 08:50:18 +0000

    Automated deployment to GitHub Pages: c75445f0c8d1b0dc813e3f9abec56ab7d598dadb

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2090,7 +2090,7 @@ var indicatorView = function (model, options) {
               return (subA > subB) - (subA < subB);
             });
 
-            var last = ''
+            var last = '';
             _.each(sorted, function(set){
               text.push('<li data-datasetindex="' + set.datasetIndex + '">');
               var subLabel = set.label.substr(set.label.indexOf(','), set.label.length);
@@ -2108,7 +2108,7 @@ var indicatorView = function (model, options) {
               if (subLabel.substr(subLabel.lastIndexOf(','), subLabel.length) != last{
                 text.push('<hr>');
               }
-              last = subLabel.substr(subLabel.lastIndexOf(','), subLabel.length)
+              last = subLabel.substr(subLabel.lastIndexOf(','), subLabel.length);
               text.push(translations.t(set.label));
               text.push('</li>');
             });
