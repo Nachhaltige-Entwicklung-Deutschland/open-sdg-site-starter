a199ac1c80c3aa03a43a207358c9f85245695641
MoBosse <moritz.bosse@destatis.de>
2019-12-16 15:25:15 +0000

    Automated deployment to GitHub Pages: ee57cc1ee6506c48ae81f095b7d699f12659c82f

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2484,7 +2484,7 @@ var indicatorSearch = function(inputElement, indicatorDataStore) {
       window.location.replace(that.inputElement.data('pageurl') + searchValue);
     }
   });
-  
+
   $("#search-btn").click(function() {
     var searchValue = that.inputElement.val();
     if(searchValue.length) {
@@ -2582,7 +2582,6 @@ $(function() {
 
 
 });
-
 $(function() {
 
   var topLevelSearchLink = $('.top-level span:eq(1), .top-level button:eq(1)');
