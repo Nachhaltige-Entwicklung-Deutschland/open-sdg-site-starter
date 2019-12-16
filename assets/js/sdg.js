689f27a8ed0ba810e6fafe8197f5a5f85722e516
MoBosse <moritz.bosse@destatis.de>
2019-12-16 16:13:31 +0000

    Automated deployment to GitHub Pages: c604dc4b5565963f5dd490adb951f4c961c7a56d

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2123,7 +2123,7 @@ var indicatorView = function (model, options) {
 
               text.push('<li data-datasetindex=" ' + set.datasetIndex + '" style:"dispaly: block">');
 
-              for (var i == 0; i<subLabel.count(','); i++){
+              for (var i==0; i<subLabel.split(",").length-1; i++){
                 text.push('<span>---</span>');
               };
 
