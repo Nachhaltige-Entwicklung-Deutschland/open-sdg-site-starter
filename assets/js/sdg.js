4464413cb411e11aeb1dc675225d31d41853f032
MoBosse <moritz.bosse@destatis.de>
2019-12-17 09:57:46 +0000

    Automated deployment to GitHub Pages: 36f4deadfc314d93a4ca10dcd04ea6bf8a700f5e

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2106,8 +2106,8 @@ var indicatorView = function (model, options) {
 
 
 
-              text.push('<li style="display: block">');
-              text.push('<li data-datasetindex="' + dataset.datasetIndex + '>'); //#18.2 >>> text.push('<li data-datasetindex="' + datasetIndex + '">');
+
+              text.push('<li id ="xxx" data-datasetindex="' + dataset.datasetIndex + '>'); //#18.2 >>> text.push('<li data-datasetindex="' + datasetIndex + '">');
 
               //---#3 targetDifferentInLegend---start----------------------------------------------------------------------------------------------------------------------------
               //text.push('<span class="swatch' + (dataset.borderDash ? ' dashed' : '') + '" style="background-color: ' + dataset.backgroundColor + '">');
@@ -2122,7 +2122,7 @@ var indicatorView = function (model, options) {
               text.push('</span>');
               text.push(translations.t(dataset.label));
               text.push('</li>');
-              text.push('</li>');
+
 
             });
 
