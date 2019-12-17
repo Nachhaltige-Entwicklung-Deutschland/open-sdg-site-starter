010f19c55d095bc9d3124271e1adbd6f1b0eec58
MoBosse <moritz.bosse@destatis.de>
2019-12-17 08:07:52 +0000

    Automated deployment to GitHub Pages: 368d3917bc3961052b13d6a9f1f4d1844d1b4257

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2080,7 +2080,7 @@ var indicatorView = function (model, options) {
         },
         legendCallback: function(chart) {
             var text = ['<ul id="legend" style="text-align: left; padding-left: 0px">'];
-            text.push('<span>');
+            //text.push('<span>');
 
             //---sort the dataset by string or substring
             var temp = [];
@@ -2110,16 +2110,18 @@ var indicatorView = function (model, options) {
               else{
                 var subLabel = set.label;
               }
-
+              //-----------------------------------------------------------
+              // replace parts that contain a comma which is no separator---
               var replace = [{old: '2,5', new: '2.5'},
                             {old: 'Gebäude-, Frei- & Betriebsfläche', new: 'Gebäude- Frei- & Betriebsfläche'}];
               for (var i=0; i<replace.length; i++){
                 subLabel = subLabel.replace(replace[i]['old'], replace[i]['new']);
               };
+              //-----------------------------------------------------------
 
-              if (subLabel.substr(0, subLabel.lastIndexOf(',')) != last){
-                text.push('</span><hr><span>');
-              }
+              //if (subLabel.substr(0, subLabel.lastIndexOf(',')) != last){
+                //text.push('</span><hr><span>');
+              //}
               last = subLabel.substr(0, subLabel.lastIndexOf(','));
 
               text.push('<li data-datasetindex=" ' + set.datasetIndex + '">');
@@ -2142,7 +2144,7 @@ var indicatorView = function (model, options) {
               text.push(translations.t(set.label));
               text.push('</li>');
             });
-            text.push('</span><hr>');
+            //text.push('</span><hr>');
             text.push('</ul>');
             return text.join('');
         },
