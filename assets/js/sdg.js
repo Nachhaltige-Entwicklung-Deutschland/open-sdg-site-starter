7cc15a2cb2e2f5014a06619b0dd36fe658065493
MoBosse <moritz.bosse@destatis.de>
2020-01-07 14:08:05 +0000

    Automated deployment to GitHub Pages: 73ba0462203327f965c5f985e753b0d0e0b10fc2

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2398,8 +2398,8 @@ var indicatorView = function (model, options) {
 
       var getHeading = function(heading, index) {
         var span = '<span class="sort" />';
-        var span_heading = '<span>' + translations.t(heading) + '</span>';
-        return (!index || heading.toLowerCase() == 'units') ? span_heading + span +'X': span + span_heading;
+        var span_heading = '<span>' + translations.t(heading) +' '+index + '</span>';
+        return (!index || heading.toLowerCase() == 'units') ? span_heading + span : span + span_heading;
       };
 
       table.headings.forEach(function (heading, index) {
