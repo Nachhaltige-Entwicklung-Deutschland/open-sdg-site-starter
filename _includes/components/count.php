c6b14cef1d48d163409637aa774b03bf8f8ee70f
MoBosse <moritz.bosse@destatis.de>
2020-01-15 15:38:42 +0100

    6.0

--- _includes/components/count.php
+++ _includes/components/count.php
@@ -1,23 +1,8 @@
 
 <?php
 
-// Simple Counter v1.0
-// Für die korrekte Funktionsweise des Counters ist darauf zu
-// achten, dass die entsprechenden Schreibrechte auf dem
-// Webserver gesetzt sind.
-
-// Counterdateiname
-$datei="counter.txt";
-
-$hits=file($datei);
-
-$hits[0]++;
-
-$fp=fopen($datei, "w");
-
-fputs($fp, "$hits[0]");
-
-fclose($fp);
-
-echo $hits[0];
+/<?php
+$zitate = file_get_contents('counter.txt');
+echo $zitate;
+?>
 ?>
