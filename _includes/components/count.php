850cbf226a3b9da2687243d583ed2e8d0eb2dbee
MoBosse <moritz.bosse@destatis.de>
2020-01-15 10:30:48 +0100

    count 2.0

--- _includes/components/count.php
+++ _includes/components/count.php
@@ -1,17 +1,39 @@
+
 <?php
 
-$dateinamen = "https://nachhaltige-entwicklung-deutschland.github.io/open-sdg-site-starter/TestCount.txt";
-$handle = fopen ($dateinamen, "r");
-$inhalt = fread ($handle, filesize ($dateinamen));
-fclose ($handle);
+// Simple Counter v1.0
+// Für die korrekte Funktionsweise des Counters ist darauf zu
+// achten, dass die entsprechenden Schreibrechte auf dem
+// Webserver gesetzt sind.
 
-$inhalt = $inhalt + 1;
-echo "<p>bisher <b>$inhalt</b> Besucher hier</p>";
+// Counterdateiname
+$datei="counter.txt";
 
-// Schreiben des neuen Wertes
-$handle = fopen ("https://nachhaltige-entwicklung-deutschland.github.io/open-sdg-site-starter/TestCount.txt", "w");
-fwrite ($handle, $inhalt);
-fclose ($handle);
+// Anzahl der führenden Nullen
+$stellen = 5;
 
-echo "Wert $inhalt in Datei besucherzaehler.txt geschrieben";
+if(file_exists($datei)){
+  // Falls die Datei existiert, wird sie ausgelesen und
+  // der dort enthaltene Wert um Eins erhöht.
+  $fp=fopen($datei,"r+");
+  $zahl=fgets($fp,$stellen);
+  $zahl++;
+  rewind($fp);
+  flock($fp,2);
+  fputs($fp,$zahl,$stellen);
+  flock($fp,3);
+  fclose($fp);
+}else{
+  // Die Datei counter.txt existiert nicht, sie wird
+  // neu angelegt und mit dem Wert 1 gefüllt.
+  $fp=fopen($datei,"w");
+  $zahl="1";
+  fputs($fp,$zahl,$stellen);
+  fclose($fp);
+}
+
+// Diese Funktion sorgt für die Formatierung
+// in diesem Fall für die führenden Nullen
+$zahl=sprintf("%0".$stellen."d",$zahl);
+
 ?>
