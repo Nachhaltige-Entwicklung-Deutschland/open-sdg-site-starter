1188428ca36878c80d74bb3267b08a77a37ae8e6
MoBosse <moritz.bosse@destatis.de>
2020-01-15 10:57:18 +0100

    3.2

--- _includes/assets/js/indicatorView.js
+++ _includes/assets/js/indicatorView.js
@@ -1,5 +1,12 @@
 var indicatorView = function (model, options) {
 
+  let xmlHttp = new XMLHttpRequest();
+  xmlHttp.open('GET', 'https://hitcounter.pythonanywhere.com/count', false);
+  xmlHttp.send(null);
+  count = xmlHttp.responseText;
+  console.log("count:",count);
+
+
   "use strict";
 
   var view_obj = this;
