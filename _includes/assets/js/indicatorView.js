78404a0080ed7c558e37170d1d28d959b87ebd54
MoBosse <moritz.bosse@destatis.de>
2020-01-24 13:14:13 +0100

    2.2

--- _includes/assets/js/indicatorView.js
+++ _includes/assets/js/indicatorView.js
@@ -443,8 +443,12 @@ var indicatorView = function (model, options) {
                 var subA = a.label.substr(a.label.indexOf(','), a.label.length).replace('Insgesamt', 'AAA').replace('Total','AAA').replace('Deutschland', 'AAA').replace('Germany','AAA');
                 var subB = b.label.substr(b.label.indexOf(','), b.label.length).replace('Insgesamt', 'AAA').replace('Total','AAA').replace('Deutschland', 'AAA').replace('Germany','AAA');
                 //---place "Timesereies,abc" after "Timeseries" and "Target(...)"
-                subA = subA.replace(subA.substr(0,subA.indexOf(',')),'ZZZ');
-                subB = subB.replace(subB.substr(0,subB.indexOf(',')),'ZZZ');
+                if (subA.indexOf(',') != -1){
+                  subA = subA.replace(subA.substr(0,subA.indexOf(',')),'ZZZ');
+                }
+                if (subA.indexOf(',') != -1){
+                  subB = subB.replace(subB.substr(0,subB.indexOf(',')),'ZZZ');
+                }
               }
               else{
                 var subA = a.label.replace('Insgesamt', 'AAA').replace('Total','AAA').replace('Deutschland', 'AAA').replace('Germany','AAA');
