774f65174ef53a276f42aa9a5edd2d0b098a8c19
MoBosse <moritz.bosse@destatis.de>
2020-01-30 13:05:55 +0000

    Automated deployment to GitHub Pages: f23bdc2d2993bc9ba929b50c98d2eaa190872646

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -50,7 +50,7 @@ opensdg.autotrack = function(preset, category, action, label) {
     maxZoom: 15,
     // Visual/choropleth considerations.
     colorRange: chroma.brewer.BuGn,
-    noValueColor: '#f0f0f0',
+    noValueColor: '#ffffff',
     styleNormal: {
       weight: 1,
       opacity: 1,
@@ -101,6 +101,7 @@ opensdg.autotrack = function(preset, category, action, label) {
     //---#2.1 caseNoTimeSeriesInCsv---start------------------------------------
     this.title = options.title;
     this.unit1 = options.measurementUnit;
+    console.log('0.1:', this.title);
     console.log('1.1:', this.unit1);
     //---#2.1 caseNoTimeSeriesInCsv---stop-------------------------------------
 
