64173b549560a9952b85d2012d17504b9cdd00c2
MoBosse <moritz.bosse@destatis.de>
2020-02-03 13:59:08 +0100

    2.1

--- _includes/assets/js/indicatorModel.js
+++ _includes/assets/js/indicatorModel.js
@@ -460,7 +460,7 @@ var indicatorModel = function (options) {
         }
       },
       //---#13 noLineForTargets---stop--------------------------------------------------------------------------------------------------
-      console.log("x:",this.indicatorId),
+
       //--#14 mixedCharts---start-------------------------------------------------------------------------------------------------------
       //barCharts = [//translations.t('a) time series')+", "+translations.t('calculated annual values'),
                   //translations.t('a) time series')+", "+translations.t('air pollutants overall'),
@@ -486,7 +486,7 @@ var indicatorModel = function (options) {
       barCharts = ['indicator_5-1-c', 'indicator_11-2-c'];
 
       getChartStyle = function (indicatorId) {
-        console.log("y:",indicatorId)
+
         if (barCharts.indexOf(indicatorId) != -1) {
           return 'bar';
         }
