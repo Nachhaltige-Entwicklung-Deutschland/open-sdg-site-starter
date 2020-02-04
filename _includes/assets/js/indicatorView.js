f52ba077617fd730db945ccbaac3e6666c3adf60
MoBosse <moritz.bosse@destatis.de>
2020-02-04 11:33:54 +0100

    6.3

--- _includes/assets/js/indicatorView.js
+++ _includes/assets/js/indicatorView.js
@@ -102,7 +102,7 @@ var indicatorView = function (model, options) {
         var fieldValue = args.minimumFieldSelections[fieldToSelect];
         //--#21 allowMultipleStartValues---start------------------------------
         //setTimeout(getClickFunction(fieldToSelect, fieldValue), 500);
-        console.log(args.minimumFieldSelections, args.minimumFieldSelections.length);
+        console.log(args.minimumFieldSelections, _.size(args.minimumFieldSelections));
         _.each(fieldValue, function(multiValue){
           setTimeout(getClickFunction(fieldToSelect, multiValue), 500);
         });
