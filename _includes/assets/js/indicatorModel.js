0a56892cd4723760d3ef288b789e43a40cf10339
MoBosse <moritz.bosse@destatis.de>
2020-02-04 08:59:40 +0100

    refresh

--- _includes/assets/js/indicatorModel.js
+++ _includes/assets/js/indicatorModel.js
@@ -790,7 +790,6 @@ var indicatorModel = function (options) {
           });
         });
       }
-
       if (_.size(minimumFieldSelections) == 0) {
         // If we did not have any pre-configured start values, we calculate them.
         // We have to decide what filters will be selected, and in some cases it
