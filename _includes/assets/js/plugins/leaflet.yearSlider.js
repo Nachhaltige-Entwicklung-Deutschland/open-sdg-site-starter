2719987d3ab05987c6651fb10d5b9311f7c1fdd3
MoBosse <moritz.bosse@destatis.de>
2020-02-06 17:15:34 +0100

    4.2

--- _includes/assets/js/plugins/leaflet.yearSlider.js
+++ _includes/assets/js/plugins/leaflet.yearSlider.js
@@ -29,7 +29,7 @@
     _getDisplayDateFormat: function(date){
       return date.getFullYear();
     }
-    
+
   });
 
   // Helper function to compose the full widget.
@@ -44,7 +44,7 @@
       times: options.years.join('-01-02,') + '-01-02',
       currentTime: new Date(options.years[0] + '-01-02').getTime(),
     });
-    console.log("ys:",options.years[-1]);
+    console.log("ys:",options.years);
     // Create the player.
     options.player = new L.TimeDimension.Player(options.playerOptions, options.timeDimension);
     // Listen for time changes.
