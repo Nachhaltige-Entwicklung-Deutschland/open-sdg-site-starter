39b75f370c925ad1a282a040a712a4400841f6e6
MoBosse <moritz.bosse@destatis.de>
2020-02-10 08:28:21 +0000

    Automated deployment to GitHub Pages: 2d40479947a7543f52b32abd8c0dc70aeebd7120

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2178,11 +2178,12 @@ var indicatorView = function (model, options) {
 
 
   this.createPlot = function (chartInfo) {
-    console.log("chartinfox",chartInfo);
+    console.log("chartinfox",chartInfo.data.datasets);
 
     var that = this;
 
-    this._model.stackedBar ? console.log("chartinfox",this._model.stackedBar) : console.log("notStacked");
+    this._model.stackedBar ? console.log("chartinfoy",this._model.stackedBar) : console.log("notStacked");
+
     var chartConfig = {
       type: this._model.graphType,
       data: chartInfo,
