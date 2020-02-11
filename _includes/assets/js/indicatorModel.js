bb6d7c146c8b94ee1aba70059f3d285c8f85f405
MoBosse <moritz.bosse@destatis.de>
2020-02-11 12:26:22 +0100

    1.2

--- _includes/assets/js/indicatorModel.js
+++ _includes/assets/js/indicatorModel.js
@@ -531,22 +531,19 @@ var indicatorModel = function (options) {
       barCharts = ['indicator_2-2-a','indicator_3-1-e','indicator_5-1-b','indicator_5-1-c','indicator_6-2-a','indicator_8-2-c','indicator_8-3-a',
       'indicator_8-4-a','indicator_8-6-a','indicator_11-1-a','indicator_11-1-b','indicator_11-2-c','indicator_12-1-a','indicator_12-1-b','indicator_13-1-b','indicator_15-2-a','indicator_16-1-a','indicator_16-2-a','indicator_17-1-a','indicator_17-2-a'];
 
+      bl = ['bw','by','be','bb','hb','hh','he','mv','ni','nw','rp','sl','sn','st','sh','th'];
+
       exceptions = [translations.t('direct co2 emissions and co2 content of consumer goods'),
                     translations.t('a) time series') + ', ' + translations.t('a) total (moving four-year average)'),
                     translations.t('b) target (max)') + ', ' + translations.t('a) total (moving four-year average)')];
 
+      for (var i=0; i<bl.length; i++){
+        exceptions.push(translations.t('a) time series') + ', ' + translations.t('a) total (moving four-year average)') + ', ' + translations.t(bl[i]));
+      };
 
 
       getChartStyle = function (indicatorId, combinationDescription) {
-        //---Test if Bundesland
-        bl = ['bw','by','be','bb','hb','hh','he','mv','ni','nw','rp','sl','sn','st','sh','th'];
-        for (var i=0; i<bl.length; i++){
-          if(combinationDescription.indexOf(translations.t(bl[i])) != -1 ){
-            var excBl = true;
-          }
-        };
-        //-----------
-
+        
         if (barCharts.indexOf(indicatorId) != -1) {
           if (exceptions.indexOf(combinationDescription) != -1 || excBl ){
             return 'line';
