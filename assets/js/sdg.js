41b055df5b617f7b219586b1637e145acba6121b
MoBosse <moritz.bosse@destatis.de>
2020-02-11 14:18:48 +0000

    Automated deployment to GitHub Pages: cdbe9be33d7acec00e56b9c0c6af46378e14f508

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -398,7 +398,7 @@ opensdg.autotrack = function(preset, category, action, label) {
               var div = L.DomUtil.create('div', 'command');
               //set the Button on position 'startExp' to status checked
               if (i == plugin.startExp){
-                div.innerHTML = '<label><input id="command'+toString(i)+'" type="radio" name="disagg" value="'+i+'" checked> '+translations.t(label)+'</label><br>';
+                div.innerHTML = '<label  style="background-color: white"><input id="command'+toString(i)+'" type="radio" name="disagg" value="'+i+'" checked> '+translations.t(label)+'</label><br>';
               }
               else{
                 div.innerHTML = '<label><input id="command'+toString(i)+'" type="radio" name="disagg" value="'+i+'"> '+translations.t(label)+'</label><br>';
