3ff90bea69ad5ee33c03cdf0aee370c68fe49bf3
MoBosse <moritz.bosse@destatis.de>
2020-02-18 15:37:17 +0100

    6.2

--- _includes/assets/js/indicatorModel.js
+++ _includes/assets/js/indicatorModel.js
@@ -200,13 +200,25 @@ var indicatorModel = function (options) {
                   'goalColors':[]};
   var shades = [0,1,-1,1/2,-1/2,3/4,-3/4,1/4,-1/4,7/8,-7/8,3/8,-3/8,5/8,-5/8,1/8,-1/8];
 
-  function LightenDarkenColor(col, amt) {
-    var num = parseInt(col, 16);
-    var r = (num >> 16) + amt;
-    var b = ((num >> 8) & 0x00FF) + amt;
-    var g = (num & 0x0000FF) + amt;
-    var newColor = g | (b << 8) | (r << 16);
-    return newColor.toString(16);
+  function shadeColor(color, percent) {
+
+    var R = parseInt(color.substring(1,3),16);
+    var G = parseInt(color.substring(3,5),16);
+    var B = parseInt(color.substring(5,7),16);
+
+    R = parseInt(R * (100 + percent) / 100);
+    G = parseInt(G * (100 + percent) / 100);
+    B = parseInt(B * (100 + percent) / 100);
+
+    R = (R<255)?R:255;
+    G = (G<255)?G:255;
+    B = (B<255)?B:255;
+
+    var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+    var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+    var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+    return RR+GG+BB;
   }
 
   if (this.colorSet == 'goalColors'){
@@ -217,11 +229,11 @@ var indicatorModel = function (options) {
     for (var num=1; num<this.numberOfColors; num++){
       var shadeNum = shades[num-1];
       if ( num % 2 == 0) {
-        var darker = LightenDarkenColor(sdgColor, shadeNum*50);
+        var darker = shadeColor(sdgColor, shadeNum*50);
         colorSets['goalColors'].push(darker);
       }
       else{
-        var lighter = LightenDarkenColor(sdgColor, shadeNum*-50);
+        var lighter = shadeColor(sdgColor, shadeNum*-50);
         colorSets['goalColors'].push(lighter);
       }
     }
