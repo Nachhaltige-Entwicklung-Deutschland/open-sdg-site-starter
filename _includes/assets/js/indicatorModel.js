044bfeee41968a09746400633c459c723b1600c7
MoBosse <moritz.bosse@destatis.de>
2020-02-20 13:19:08 +0100

    2.4

--- _includes/assets/js/indicatorModel.js
+++ _includes/assets/js/indicatorModel.js
@@ -215,7 +215,7 @@ var indicatorModel = function (options) {
   var colorSets = {'default':['7e984f', '8d73ca', 'aaa533', 'c65b8a', '4aac8d', 'c95f44'],
                   'sdgColors':['e5243b', 'DDA63A', '4c9f38', 'c5192d', 'ff3a21', '26bde2', 'fcc30b', 'a21942', 'fd6925', 'dd1367','FD9D24','BF8B2E','3F7E44','0A97D9','56C02B','00689D','19486A'],
                   'goalColors': goalColors[goalNumber-1],
-                  'customColors': {{ site.customColors | jsonify }}};
+                  'customColors': {{ site.customColorList | jsonify }}};
 
   this.numberOfColors = {{ site.numberOfColors | jsonify }}>colorSets[this.colorSet].length ? colorSets[this.colorSet].length : {{ site.numberOfColors | jsonify }};
 
