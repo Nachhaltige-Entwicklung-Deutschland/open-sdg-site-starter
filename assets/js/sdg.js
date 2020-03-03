2511599ac3b1a58ae33b211cfa4d8f2ab73bdaa5
MoBosse <moritz.bosse@destatis.de>
2020-03-03 09:16:09 +0000

    Automated deployment to GitHub Pages: e578d8c10466fe78ce46eeea576f712e37f47b0b

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1066,7 +1066,7 @@ var indicatorDataStore = function(dataUrl) {
   // this.numberOfColors = null>colorSets[this.colorSet].length ? colorSets[this.colorSet].length : null;
   //
   // var colors = colorSets[this.colorSet].slice(0,this.numberOfColors);
-  var colorOps = new chartColors(this.indicatorId);
+  var colorOps = opensdg.chartColors(this.indicatorId);
   var colors = colorOps.colors;
   console.log("z", colors);
 
