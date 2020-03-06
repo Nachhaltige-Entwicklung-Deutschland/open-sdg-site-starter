eb108b0c344284063d8795607ba68557f768c274
MoBosse <moritz.bosse@destatis.de>
2020-03-06 08:36:49 +0100

    test colorSets

--- _includes/assets/js/chartColors.js
+++ _includes/assets/js/chartColors.js
@@ -4,7 +4,7 @@
     this.indicatorId = indicatorId;
     this.colorSet = colorSet;
 
-    this.goalNumber = parseInt(this.indicatorId.slice(this.indicatorId.indexOf('_')+1,this.indicatorId.indexOf('-')));
+    this.goalNumber = parseInt(indicatorId.slice(indicatorId.indexOf('_')+1,indicatorId.indexOf('-')));
     console.log("b, goalNumber", this.goalNumber);
 
     this.goalColors = [['e5243b', '891523', 'ef7b89', '2d070b', 'f4a7b0', 'b71c2f', 'ea4f62', '5b0e17', 'fce9eb'],
@@ -34,5 +34,6 @@
     this.colors = colorSets[this.colorSet].slice(0,this.numberOfColors);
 
     console.log("c, colors", this.colors);
+    return this.colors;
 
 };
