c7789188409b5e4208b7f9ae4361a3cae4a178aa
MoBosse <moritz.bosse@destatis.de>
2021-02-05 16:39:06 +0000

    Automated deployment to GitHub Pages: 01839972df9819e882957bc44bff15a7f6369c64

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1485,18 +1485,18 @@ var indicatorDataStore = function(dataUrl) {
             //--#14.1 barsOnly---start------------------------------------------------
             type: getChartStyle(that.indicatorId, combinationDescription),
             //--#14.1 barsOnly---stop-------------------------------------------------
-            options:{
-                scales: {
-                    xAxes: [{
-                        stacked:  getStacked(that.indicatorId)
-                    }],
-                    yAxes: [{
-                        stacked: getStacked(that.indicatorId)
-                    }]
-                  }
-                },
-            //stacked: getStacked(that.indicatorId),
-            stack: getStackGroup(that.indicatorId),
+            // options:{
+            //     scales: {
+            //         xAxes: [{
+            //             stacked:  getStacked(that.indicatorId)
+            //         }],
+            //         yAxes: [{
+            //             stacked: getStacked(that.indicatorId)
+            //         }]
+            //       }
+            //     },
+            // //stacked: getStacked(that.indicatorId),
+            // stack: getStackGroup(that.indicatorId),
             borderWidth: combinationDescription ? 2 : 4
           }, that.datasetObject);
         console.log(ds);
