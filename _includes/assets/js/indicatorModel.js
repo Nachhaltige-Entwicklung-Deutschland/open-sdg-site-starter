8040d8fdd22a66e7ee0e226dfa8d3350b18e8484
MoBosse <moritz.bosse@destatis.de>
2021-02-05 09:37:54 +0100

    refrsh

--- _includes/assets/js/indicatorModel.js
+++ _includes/assets/js/indicatorModel.js
@@ -491,7 +491,7 @@ var indicatorModel = function (options) {
       },
       //---#22 xxx---stop--------------------------------------------------------------------------------------------------
 
-      stackedCharts = ['indicator_12-1-b'];
+      stackedCharts = ['indicator_3-3-a'];
       getStacked = function(indicatorId){
         if (stackedCharts.indexOf(indicatorId) != -1) {
           return true;
@@ -524,8 +524,8 @@ var indicatorModel = function (options) {
       //--#14 mixedCharts---stop--------------------------------------------------------------------------------------------------------
 
       //--#14.1 barsOnly---start--------------------------------------------------------------------------------------------------------
-      barCharts = ['indicator_2-2-a','indicator_3-1-e','indicator_5-1-b','indicator_5-1-c','indicator_6-2-a','indicator_8-2-c','indicator_8-3-a',
-      'indicator_8-4-a','indicator_8-6-a','indicator_11-1-a','indicator_11-1-b','indicator_11-2-c','indicator_12-1-a','indicator_12-1-b','indicator_15-2-a','indicator_16-1-a','indicator_16-2-a','indicator_17-1-a','indicator_17-2-a'];
+      barCharts = ['indicator_2-2-a','indicator_3-1-e','indicator_3-3-a','indicator_5-1-b','indicator_5-1-c','indicator_5-1-e','indicator_6-2-ab','indicator_8-2-c','indicator_8-3-a',
+      'indicator_8-4-a','indicator_8-6-a','indicator_11-1-a','indicator_11-1-b','indicator_11-2-c','indicator_12-1-a','indicator_13-1-a','indicator_15-2-a','indicator_16-1-a','indicator_16-2-a','indicator_17-1-a','indicator_17-2-a'];
 
       bl = ['bw','by','be','bb','hb','hh','he','mv','ni','nw','rp','sl','sn','st','sh','th'];
 
