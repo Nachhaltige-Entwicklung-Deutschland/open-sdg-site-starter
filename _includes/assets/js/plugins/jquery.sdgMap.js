613b696f721aee12ced0a493badfef2dc8c43c8c
MoBosse <moritz.bosse@destatis.de>
2021-02-26 08:42:18 +0100

    opacity 2

--- _includes/assets/js/plugins/jquery.sdgMap.js
+++ _includes/assets/js/plugins/jquery.sdgMap.js
@@ -332,7 +332,6 @@
         minZoom: this.options.minZoom,
         maxZoom: this.options.maxZoom,
         zoomControl: false,
-        opacity: 1.0
       });
       this.map.setView([51.9, 10.26],0);
       this.dynamicLayers = new ZoomShowHide();
