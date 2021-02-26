5e701cb2a5117eccbd385eba1ab14e3825d62c68
MoBosse <moritz.bosse@destatis.de>
2021-02-26 07:40:10 +0000

    Automated deployment to GitHub Pages: c07c46414a65182e9ec6958a5cc68960a3c0b941

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -363,7 +363,7 @@ opensdg.autotrack = function(preset, category, action, label) {
         minZoom: this.options.minZoom,
         maxZoom: this.options.maxZoom,
         zoomControl: false,
-        opacity: 0.5
+        opacity: 1.0
       });
       this.map.setView([51.9, 10.26],0);
       this.dynamicLayers = new ZoomShowHide();
