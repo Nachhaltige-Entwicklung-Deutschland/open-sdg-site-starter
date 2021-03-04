981969dc77249a62eb634dabc7f258c49c3e4578
MoBosse <moritz.bosse@destatis.de>
2021-03-04 13:32:19 +0000

    Automated deployment to GitHub Pages: 9cb28dddc16eba8ed894e0173bb555dba0a14e56

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1840,7 +1840,7 @@ var mapView = function () {
   //this.initialise = function(geoData, geoCodeRegEx) {
   //this.initialise = function(geoData, geoCodeRegEx, goal) {
   //---#1 GoalDependendMapColor---stop---------------------------------------
-  this.initialise = function(geoData, geoCodeRegEx, goal, title, measurementUnit) {
+  this.initialise = function(geoData, geoCodeRegEx, goal, title, measurementUnit, args.mapTitle) {
   //---#2.1 caseNoTimeSeriesInCsv---stop-------------------------------------
     $('.map').show();
     $('#map').sdgMap({
@@ -1855,7 +1855,7 @@ var mapView = function () {
       //---#2.2 footerUnitInMapLegend---start----------------------------------------------------------
       measurementUnit: measurementUnit,
       //---#2.2 footerUnitInMapLegend---stop-----------------------------------------------------------
-
+      mapTitle: mapTitle,
 
       title: title
     });
@@ -2018,7 +2018,7 @@ var indicatorView = function (model, options) {
       //view_obj._mapView.initialise(args.geoData, args.geoCodeRegEx, goalNr);
       //---#1 GoalDependendMapColor---stop---------------------------
       console.log("Args: ", args);
-      view_obj._mapView.initialise(args.geoData, args.geoCodeRegEx, goalNr, args.title, args.measurementUnit); //---#2.2 footerUnitInMapLegend
+      view_obj._mapView.initialise(args.geoData, args.geoCodeRegEx, goalNr, args.title, args.measurementUnit, args.mapTitle); //---#2.2 footerUnitInMapLegend
       //---#2 TimeSeriesNameDisplayedInMaps---stop------------------
 
     }
