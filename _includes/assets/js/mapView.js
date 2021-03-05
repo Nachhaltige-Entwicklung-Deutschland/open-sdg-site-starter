6da2a1c83ef1eefef664fcd3c5aa9d69861a8556
MoBosse <moritz.bosse@destatis.de>
2021-03-05 10:21:56 +0100

    14

--- _includes/assets/js/mapView.js
+++ _includes/assets/js/mapView.js
@@ -6,7 +6,7 @@ var mapView = function () {
   //this.initialise = function(geoData, geoCodeRegEx) {
   //this.initialise = function(geoData, geoCodeRegEx, goal) {
   //---#1 GoalDependendMapColor---stop---------------------------------------
-  this.initialise = function(geoData, geoCodeRegEx, goal, title, measurementUnit) { //, mapTitle
+  this.initialise = function(geoData, geoCodeRegEx, goal, title, measurementUnit, mapTitle) { //, mapTitle
   //---#2.1 caseNoTimeSeriesInCsv---stop-------------------------------------
     $('.map').show();
     $('#map').sdgMap({
@@ -21,7 +21,7 @@ var mapView = function () {
       //---#2.2 footerUnitInMapLegend---start----------------------------------------------------------
       measurementUnit: measurementUnit,
       //---#2.2 footerUnitInMapLegend---stop-----------------------------------------------------------
-      //mapTitle: mapTitle,
+      mapTitle: mapTitle,
 
       title: title
     });
