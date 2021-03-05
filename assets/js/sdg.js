d2e8510e6bed0a26266dcbd48c442eda7f12c0ae
MoBosse <moritz.bosse@destatis.de>
2021-03-05 14:30:33 +0000

    Automated deployment to GitHub Pages: ef92998a30ebff97b27561e5a5672eb5b5ee094c

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1542,13 +1542,16 @@ var indicatorDataStore = function(dataUrl) {
             pointRadius: getRadius(datasetIndex, combinationDescription),
             pointBorderColor: '#' + getColor(datasetIndexMod),
             borderDash: getBorderDash(datasetIndex, combinationDescription),
+
             // data: _.map(that.years, function (year) {
             //   var found = _.findWhere(data, {
             //     Year: year
             //   });
             //   return found ? found.Value : null;
             // }),
-            data: years.map(function(year, index) {
+
+
+            data: _.map(that.years, function(year, index) {
               return [year].concat(datasets.map(function(ds) {
                 if (typeof ds.data[index] === 'undefined') {
                   return null;
