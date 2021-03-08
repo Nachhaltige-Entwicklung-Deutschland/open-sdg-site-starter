eea946c4c2231dd4ef066b8600f1303a0dfeffe5
MoBosse <moritz.bosse@destatis.de>
2021-03-08 07:28:27 +0000

    Automated deployment to GitHub Pages: ae15cd13b0514f643f75563a1d6940dc9a2d60c1

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1427,7 +1427,7 @@ var indicatorDataStore = function(dataUrl) {
 
       //--#14.1 barsOnly---start--------------------------------------------------------------------------------------------------------
       barCharts = ['indicator_2-1-a', 'indicator_2-2-a','indicator_3-1-e','indicator_3-2-a','indicator_3-3-a','indicator_5-1-b','indicator_5-1-c','indicator_5-1-e','indicator_6-2-ab','indicator_8-2-ab', 'indicator_8-2-c','indicator_8-3-a',
-      'indicator_8-4-a','indicator_8-6-a','indicator_11-1-a','indicator_11-1-b','indicator_11-2-c','indicator_12-1-a','indicator_13-1-a','indicator_15-2-a','indicator_16-1-a','indicator_16-2-a','indicator_17-1-a','indicator_17-2-a'];
+      'indicator_8-4-a','indicator_8-6-a','indicator_11-1-a','indicator_11-1-b','indicator_11-2-c','indicator_12-1-a','indicator_13-1-a','indicator_15-1-a','indicator_15-2-a','indicator_16-1-a','indicator_16-2-a','indicator_17-1-a','indicator_17-2-a'];
 
       bl = ['bw','by','be','bb','hb','hh','he','mv','ni','nw','rp','sl','sn','st','sh','th'];
 
@@ -1446,7 +1446,13 @@ var indicatorDataStore = function(dataUrl) {
                     translations.t('a) time series') + ', ' + translations.t('nox'),//3.2.a
                     translations.t('a) time series') + ', ' + translations.t('nh3'),//3.2.a
                     translations.t('a) time series') + ', ' + translations.t('nmvoc'),//3.2.a
-                    translations.t('a) time series') + ', ' + translations.t('pm2.5')];//3.2.a
+                    translations.t('a) time series') + ', ' + translations.t('pm2.5'),//3.2.a
+                    translations.t('a) time series') + ', ' + translations.t('sub-index forests'),//15.1.a
+                    translations.t('a) time series') + ', ' + translations.t('sub-index farmland'),//15.1.a
+                    translations.t('a) time series') + ', ' + translations.t('sub-index settlements'),//15.1.a
+                    translations.t('a) time series') + ', ' + translations.t('sub-index inland waters'),//15.1.a
+                    translations.t('a) time series') + ', ' + translations.t('sub-index coasts/seas')];//15.1.a
+
 
 
       for (var i=0; i<bl.length; i++){
