30a7fdcdf6e0ec431332f00d9194c6f36e3930d3
MoBosse <moritz.bosse@destatis.de>
2021-03-09 17:46:49 +0100

    2.5

--- _includes/assets/js/indicatorModel.js
+++ _includes/assets/js/indicatorModel.js
@@ -551,8 +551,8 @@ var indicatorModel = function (options) {
                     translations.t('b) target (min)') + ', ' + translations.t('a) drinking water and sanitation'),//6.2.ab
                     translations.t('b) target (min)') + ', ' + translations.t('b) drinking water'),//6.2.ab
                     translations.t('b) target (min)') + ', ' + translations.t('c) sanitation'),//6.2.ab
-                    translations.t('a) time series') + ', ' + translations.t('b) moving five-year average, referring to the middle year'),//2.1.a
-                    translations.t('b) target (max)') + ', ' + translations.t('b) moving five-year average, referring to the middle year'),//2.1.a
+                    translations.t('a) time series') + ', ' + translations.t('a) moving five-year average, referring to the middle year'),//2.1.a
+                    translations.t('b) target (max)') + ', ' + translations.t('a) moving five-year average, referring to the middle year'),//2.1.a
                     translations.t('a) time series') + ', ' + translations.t('so2'),//3.2.a
                     translations.t('a) time series') + ', ' + translations.t('nox'),//3.2.a
                     translations.t('a) time series') + ', ' + translations.t('nh3'),//3.2.a
