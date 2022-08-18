d28c7140765880a57981784163815980b29eb131
MoBosse <moritz.bosse@destatis.de>
2022-08-18 07:36:33 +0000

    Automated deployment to GitHub Pages: ac28b00b76d3fd44ecb472e20a5aa5bf99d09658

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1450,13 +1450,14 @@ var indicatorDataStore = function(dataUrl) {
                     translations.t('a) time series') + ', ' + translations.t('nh3'),//3.2.a
                     translations.t('a) time series') + ', ' + translations.t('nmvoc'),//3.2.a
                     translations.t('a) time series') + ', ' + translations.t('pm2.5'),//3.2.a
-                    translations.t('a) time series') + ', ' + translations.t('a) sub-index forests'),//15.1.a
+                    translations.t('a) time series') + ' ' + translations.t('a) sub-index forests'),//15.1.a
                     translations.t('a) time series') + ', ' + translations.t('c) sub-index farmland'),//15.1.a
                     translations.t('a) time series') + ', ' + translations.t('b) sub-index settlements'),//15.1.a
                     translations.t('a) time series') + ', ' + translations.t('d) sub-index inland waters'),//15.1.a
                     translations.t('a) time series') + ', ' + translations.t('e) sub-index coasts/seas'),
                     translations.t('b) target (min)') + ', ' + translations.t('f) index overall'),
                     translations.t('a) time series') + ', ' + translations.t('proportion of sustainable managed stocks in all msy examined stocks'),
+                    translations.t('b)  target (min)') + ', ' + translations.t('proportion of sustainable managed stocks in all msy examined stocks'),
                     translations.t('a) time series') + ', ' + translations.t('proportion of sustainable managed stocks in all msy examined stocks') + ', ' + translations.t('north sea'),
                     translations.t('a) time series') + ', ' + translations.t('proportion of sustainable managed stocks in all msy examined stocks') + ', ' + translations.t('baltic sea')];//14.1.b
                     // translations.t('a) time series') + ', ' + translations.t('sub-index forests'),//15.1.a,
@@ -2739,7 +2740,12 @@ var indicatorView = function (model, options) {
       });
 
       //---Edit from 26.10.2020: Add Unit to table headings
-      currentTable.append('<caption>' + that._model.chartTitle +  tableUnit + '</caption>');
+      if(tableUnit{
+        currentTable.append('<caption>' + that._model.chartTitle +  tableUnit + '</caption>');
+      } else {
+        currentTable.append('<caption>' + that._model.chartTitle + '</caption>');
+      }
+      //currentTable.append('<caption>' + that._model.chartTitle +  tableUnit + '</caption>');
 
       var table_head = '<thead><tr>';
 
