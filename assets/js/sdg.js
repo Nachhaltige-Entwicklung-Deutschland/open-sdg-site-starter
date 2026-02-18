38dce548baea8d58a86aca3fe8caabfbe9f0d0b7
MoBosse <50657629+MoBosse@users.noreply.github.com>
2026-02-18 08:53:51 +0000

    Deploying to gh-pages from  @ e0d5797e288d66250a59a8469c751dc5ac3e0cbf 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2131,6 +2131,19 @@ function getDataBySelectedFields(rows, selectedFields) {
 }
 
 /**
+ * @param {Array} rows
+ * @param {Array} selectedFields Field items
+ * @return {Array} Rows
+ */
+function hasDataBySelectedFields(rows, selectedFields) {
+  return rows.some(function(row) {
+    return selectedFields.some(function(field) {
+      return field.values.includes(row[field.field]);
+    });
+  });
+}
+
+/**
  * @param {Array} fieldNames
  * @param {Object} dataSchema
  */
@@ -4605,10 +4618,10 @@ opensdg.chartTypes.base = function(info) {
         value = parseInt(value, 10);
     }
     if (value === 1) {
-        return 'Yes';
+        return translations.indicator.affirmative;
     }
     else if (value === -1) {
-        return 'No';
+        return translations.indicator.negative;
     }
     return '';
 }
